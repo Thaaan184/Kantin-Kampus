b759e227c988c425f00559dfc6ea03560591b019
wikihow182 <dwikhideandra@gmail.com>
2024-06-18 21:39:06 +0700

    footer minor update

--- report-review.php
+++ report-review.php
@@ -168,7 +168,8 @@ $query = $koneksi->query($sql);
           </div>
             </p>
         </div>
-    </footer>"https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    </footer>
+    <script>"https://code.jquery.com/jquery-2.1.3.min.js"</script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 
